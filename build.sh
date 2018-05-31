e191fd6e144cfb0d5fbb1f46109e0a4c84a630a7
Justin Mimbs <justin.mimbs@gmail.com>
2018-05-30 22:07:38 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,14 +17,16 @@ tzdata="$(pwd -P)/tzdata/$version"
 output="$(pwd -P)/dist/$version"
 
 if [ -d "$output" ]; then
+    echo ""
     echo "Existing build at 'dist/$version' is current."
+    echo ""
     exit 0
 fi
 
 # make tzdata
 
-git -C tz checkout $version
-make -C tz install_data TOPDIR="$tzdata" REDO=posix_only
+git -C tz -c advice.detachedHead=false checkout $version
+make -C tz install_data TOPDIR="$tzdata" REDO=posix_only > /dev/null
 
 zoneinfo="$tzdata/usr/share/zoneinfo"
 cp tz/zone.tab "$zoneinfo/zone.tab"
@@ -44,10 +46,21 @@ fi
 # build dist
 
 ./build.py "$zoneinfo" "$output"
+echo ""
 echo "Created new build at 'dist/$version'."
+echo ""
 
-# make tests
+# make tests, compare/main.js
 
 ./maketests.py 1000 > elm/Tests/Local.elm
-echo "To run tests, start 'elm reactor' and open:"
-echo "http://localhost:8000/elm/Tests.elm"
+
+if [ -x "$(command -v elm)" ]; then
+    elm make elm/compare/Main.elm --output elm/compare/main.js > /dev/null
+fi
+
+echo "To run tests, see examples, and compare to browser, start 'elm reactor' and open:
+
+    http://localhost:8000/elm/Tests.elm
+    http://localhost:8000/elm/Examples.elm
+    http://localhost:8000/elm/compare/index.html
+"
--- compare.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-elm make elm/compare/Main.elm --output elm/compare/main.js
-echo "To compare offset changes used by JS Date and tzdb, start 'elm reactor' and open:"
-echo "http://localhost:8000/elm/compare/index.html"
